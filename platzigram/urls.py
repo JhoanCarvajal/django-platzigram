e274c6f263f07deb5b2a8094b109bd93868c06a9
jhoan carvajal <caballoy45@gmail.com>
2019-11-28 18:58:56 -0500

    se creo una tabla en sqlite3 haciendo la base de datos

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,11 +1,15 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
-from platzigram import views
+from platzigram import views as local_views
+from posts import views as posts_views
+
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('hello-world/', views.hello_world),
-    path('sorted/', views.sort_integers),
-    path('hi/<str:name>/<int:age>/', views.say_hi),
+    path('hello-world/', local_views.hello_world),
+    path('sorted/', local_views.sort_integers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+
+    path('posts/', posts_views.list_posts)
 ]
 #agregaddo a git
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from datetime import datetime
+posts = [
+    {
+        'title': 'mont blac',
+        'user': {
+            'name': 'jhoan carvajal',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/200/200?image=1036',
+    },
+    {
+        'title': 'via lactea',
+        'user': {
+            'name': 'jhoan carvajal',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/200/200?image=903',
+    },
+    {
+        'title': 'nuevo auditorio',
+        'user': {
+            'name': 'jhoan carvajal',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/200/200?image=1076',
+    },
+]
 
-# Create your views here.
+
+
+def list_posts(request):
+
+    return render(request, 'feed.html', {'posts': posts})
