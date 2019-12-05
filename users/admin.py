3d244f9f9113cac3eea034764b965f826376bed6
jhoan carvajal <caballoy45@gmail.com>
2019-12-05 18:41:48 -0500

    edite admin del y mas cosas 05 12 2019

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 # Register your models here.
-
+from django.contrib.auth.models import User
 from users.models import Profile
 
 @admin.register(Profile)
@@ -22,5 +23,30 @@ class ProfileAdmin(admin.ModelAdmin):
                     ('website', 'phone_number'),
                     ('biography')
                 )
+            }),
+            ('Metadata', {
+                'fields': (('created', 'modified'),),
             })
     )
+
+    readonly_fields = ('created', 'modified', )
+
+class ProfileInline (admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
